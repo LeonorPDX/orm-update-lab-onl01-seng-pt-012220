43a248649cf73ecab2a64d40524276458b86ba4b
Leonor Colbert <leonoracolbert@gmail.com>
2020-04-01 18:40:28 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -56,6 +56,15 @@ class Student
     student = self.new(row[1], row[2], row[0])
     student
   end
+  
+  def self.find_by_name(name)
+    sql = <<-SQL 
+      SELECT * FROM students
+      WHERE name = ?
+    SQL
+    row = DB[:conn].execute(sql, name)
+    self.new_from_db(row)
+  end
 
 
 end
