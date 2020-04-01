340681a9c8ab4b956716df7207fad27bb2f79381
Leonor Colbert <leonoracolbert@gmail.com>
2020-04-01 18:30:29 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,11 +12,19 @@ class Student
   end
   
   def self.create_table
-    
+    sql =  <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade INTEGER
+        )
+        SQL
+    DB[:conn].execute(sql)
   end
   
   def self.drop_table
-    
+    sql = "DROP TABLE IF EXISTS students"
+    DB[:conn].execute(sql)
   end
   
   def save
@@ -26,7 +34,7 @@ class Student
     SQL
  
     DB[:conn].execute(sql, self.name, self.grade)
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
 
 
